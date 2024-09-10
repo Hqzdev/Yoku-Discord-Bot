731c9a191551b357512388520fc1e4737608d227
Haqz.dev <105918255+Hqzdev@users.noreply.github.com>
2024-09-10 20:17:40 +0500

    made new system

--- src/Event/messageCreate/giveUserXp.js
+++ src/Event/messageCreate/giveUserXp.js
@@ -2,7 +2,7 @@ const { Client, Message } = require('discord.js');
 const calculateLevelXp = require('../../utils/calculateLevelXp');
 const Level = require('../../models/Level');
 const cooldowns = new Set();
-
+ 
 function getRandomXp(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
