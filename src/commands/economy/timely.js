d98e077fc4f402b6c297d90ab4e8fb668f251ab3
Haqz.dev <105918255+Hqzdev@users.noreply.github.com>
2024-09-10 22:12:20 +0500

    fix bugs

--- src/commands/economy/timely.js
+++ src/commands/economy/timely.js
@@ -1,22 +1,26 @@
-const { Client, Interaction } = require('discord.js');
+const { Client, Interaction, EmbedBuilder } = require('discord.js');
 const User = require('../../models/User');
 
-const dailyAmount = 1000;
+const dailyAmountMin = 500; // Minimum daily amount
+const dailyAmountMax = 1500; // Maximum daily amount
+const cooldownDuration = 10800000; // 3 hours in milliseconds
 
 module.exports = {
   name: 'timely',
   description: 'Collect your money!',
-  /**
+  /*
    *
    * @param {Client} client
    * @param {Interaction} interaction
    */
   callback: async (client, interaction) => {
     if (!interaction.inGuild()) {
-      interaction.reply({
-        content: 'You can only run this command inside a server.',
-        ephemeral: true,
-      });
+      const embed = new EmbedBuilder()
+        .setColor('#303135')
+        .setDescription('You can only run this command inside a server.')
+        .setImage('https://media.discordapp.net/attachments/1014116819667259442/1114468611780784158/InShot_20230601_210530350.jpg?ex=66de0706&is=66dcb586&hm=2dd2199c9466aaf2e13089023f7c7ba612d1f3972b93eba5f1c941444a55f6bd&=&format=webp&width=1440&height=20');
+      
+      interaction.reply({ embeds: [embed], ephemeral: true });
       return;
     }
 
@@ -30,17 +34,23 @@ module.exports = {
 
       let user = await User.findOne(query);
 
-      if (user) {
-        const lastDailyDate = user.lastDaily.toDateString();
-        const currentDate = new Date().toDateString();
+      // Calculate cooldown
+      const lastDaily = user ? user.lastDaily : new Date(0);
+      const cooldownRemaining = cooldownDuration - (Date.now() - lastDaily.getTime());
 
-        if (lastDailyDate === currentDate) {
-          interaction.editReply(
-            'You have already collected your dailies today. Come back tomorrow!'
-          );
-          return;
-        }
-        
+      if (cooldownRemaining > 0) {
+        const embed = new EmbedBuilder()
+          .setColor('#303135')
+          .setDescription(`> You can collect your reward in **${Math.round(cooldownRemaining / 3600000)}h.** `)
+          .setImage('https://media.discordapp.net/attachments/1014116819667259442/1114468611780784158/InShot_20230601_210530350.jpg?ex=66de0706&is=66dcb586&hm=2dd2199c9466aaf2e13089023f7c7ba612d1f3972b93eba5f1c941444a55f6bd&=&format=webp&width=1440&height=20');
+      
+        interaction.editReply({ embeds: [embed] });
+        return;
+      }
+
+      const randomDailyAmount = Math.floor(Math.random() * (dailyAmountMax - dailyAmountMin + 1)) + dailyAmountMin;
+
+      if (user) {
         user.lastDaily = new Date();
       } else {
         user = new User({
@@ -49,12 +59,15 @@ module.exports = {
         });
       }
 
-      user.balance += dailyAmount;
+      user.balance += randomDailyAmount;
       await user.save();
 
-      interaction.editReply(
-        `${dailyAmount} was added to your balance. Your new balance is ${user.balance}`
-      );
+      const embed = new EmbedBuilder()
+        .setColor('#303135')
+        .setDescription(`> You recived **${randomDailyAmount}** <:Utility:1268981376401805404> | Come back in **${Math.round(cooldownRemaining / 3600000)}h. **`)
+        .setImage('https://media.discordapp.net/attachments/1014116819667259442/1114468611780784158/InShot_20230601_210530350.jpg?ex=66de0706&is=66dcb586&hm=2dd2199c9466aaf2e13089023f7c7ba612d1f3972b93eba5f1c941444a55f6bd&=&format=webp&width=1440&height=20');
+      
+      interaction.editReply({ embeds: [embed] });
     } catch (error) {
       console.log(`Error with /timely: ${error}`);
     }
