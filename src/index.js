9a328d9bfcad684e666678b73f136cfbb69e0bff
Haqz.dev <105918255+Hqzdev@users.noreply.github.com>
2024-09-19 17:28:35 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,123 +9,115 @@ const client = new Client({
     IntentsBitField.Flags.GuildMembers,
     IntentsBitField.Flags.GuildMessages,
     IntentsBitField.Flags.MessageContent,
+    IntentsBitField.Flags.GuildPresences,
   ],
 });
-const LOG_CHANNEL_ID = '1014116819667259442'; 
-
-let status = [
-  {
-    name: '⌨️ | Harmony Ai Bot',
-    type: ActivityType.Playing,
-  },
-  {
-    name: '💻 | Made by Haqz.dev',
-    type: ActivityType.Playing,
-  },
-  {
-    name: '🌙 | version 1.1 ',
-    type: ActivityType.Playing,
-  }
-];
-
-module.exports = {
-  name: Events.ClientReady,
-  once: true,
-  execute(client) {
-    console.log(`✅ | Yoku was made by Hqz.dev`);
-
-    let currentStatusIndex = 0;
-
-    setInterval(() => {
-      const currentStatus = status[currentStatusIndex];
-      client.user.setActivity(currentStatus.name, { type: currentStatus.type }); // Установка статуса
-      currentStatusIndex = (currentStatusIndex + 1) % status.length; // Переход к следующему статусу
-    }, 2500);
-  },
-};
 
 
+
+const LOG_CHANNEL_ID = '1011160636979429386'; 
+const CRASH_CHANNEL_ID = '1014116819667259442'
+const WELCOME = '1010967528723451996'
+const goodbye = '1011160636979429386'
+
+client.on('guildMemberUpdate', async (member) => {
+  const logs = await client.channels.fetch(LOG_CHANNEL_ID);
+  const embed = new EmbedBuilder()
+    .setColor('303135')
+    .setTitle('Harmony 🌙')
+    .setDescription('**Successfully | ✅** Thank you so much for boosting the server!\nWhat does this give you?\n__1.__ You now have access to view the audit log\n__2.__ You can now create a custom role for **3 weeks**\n__3.__ You can also post your codes in the channel <#1010981292126511296>\n__4.__ Additionally, you have been given the role <@&1012034055346331659> with special permissions')
+  logs.send({ embeds: [embed] });
+});
 
 client.on('guildMemberAdd', async (member) => {
-  const logChannel = await client.channels.fetch(LOG_CHANNEL_ID);
+  const welcome = await client.channels.fetch(WELCOME);
   const embed = new EmbedBuilder()
-      .setColor('#303135') // Зеленый цвет для присоединения
+      .setColor('#303135') 
       .setTitle('Harmony 🌙')
-      .setDescription(`<:emoji_19:1011524159702048819>**Добро пожаловать ${member} на уникальный сервер ${member.guild.name}** 
-**Твой статус** - ${member.status}
-**Дата регистрации** - ${member.user.createdAt}
-**К нам пришел** - ${member.joinedAt}
-**Также прочитай наши [Правила](https://discord.gg/rP5EekXj) и [Роли](https://discord.gg/eZNjaQTA)**
-__**Мы рады тебя видеть на нашем сервере**__`)
-      .setImage('https://media.discordapp.net/attachments/1014116819667259442/1114468611780784158/InShot_20230601_210530350.jpg?ex=66de0706&is=66dcb586&hm=2dd2199c9466aaf2e13089023f7c7ba612d1f3972b93eba5f1c941444a55f6bd&=&format=webp&width=1440&height=20');
-      
-  logChannel.send({ embeds: [embed] });
+      .setDescription(`<:emoji_19:1011524159702048819> **Welcome, ${member}, to the unique server ${member.guild.name}!** 
+**Your status** - ${member.status}
+**Account created on** - ${member.user.createdAt}
+**Joined us on** - ${member.joinedAt}
+**Also, check out our [Rules](https://discord.gg/rP5EekXj) and [Roles](https://discord.gg/eZNjaQTA)**
+__**We're glad to see you on our server!**__`);
+  welcome.send({ embeds: [embed] });
 });
-
 
 client.on('guildMemberRemove', async (member) => {
-  const logChannel = await client.channels.fetch(LOG_CHANNEL_ID);
+  const goodbyeID = await client.channels.fetch(goodbye);
   const embed = new EmbedBuilder()
-      .setColor('#303135') // Красный цвет для ухода
+      .setColor('#303135')
       .setTitle('Harmony 🌙')
-      .setDescription(` ${member}, Жаль что ты ушел с сервера
-Если тебе станет скучно приходи(`)
-.setImage('https://media.discordapp.net/attachments/1014116819667259442/1114468611780784158/InShot_20230601_210530350.jpg?ex=66de0706&is=66dcb586&hm=2dd2199c9466aaf2e13089023f7c7ba612d1f3972b93eba5f1c941444a55f6bd&=&format=webp&width=1440&height=20');
-      
-  logChannel.send({ embeds: [embed] });
+      .setDescription(`${member}, we're sad to see you leave the server. 
+If you ever feel bored, you're welcome to come back!`);
+  goodbyeID.send({ embeds: [embed] });
 });
 
+
+
+
+/**const OWNER_ID = '1268562577080713282'; 
+const BOT_ID = '1281304366153859125';
 client.on('channelCreate', async (channel) => {
-  const logChannel = await client.channels.fetch(LOG_CHANNEL_ID);
-  const embed = new EmbedBuilder()
-  .setColor('#303135')  // Синий цвет для создания канала
-      .setTitle('Новый канал')
-      .setDescription(`📢| Created new channel by ${member.name}`)
-      .setImage('https://media.discordapp.net/attachments/1014116819667259442/1114468611780784158/InShot_20230601_210530350.jpg?ex=66de0706&is=66dcb586&hm=2dd2199c9466aaf2e13089023f7c7ba612d1f3972b93eba5f1c941444a55f6bd&=&format=webp&width=1440&height=20');
-      
-  logChannel.send({ embeds: [embed] });
-});
+    const crash = await client.channels.fetch(CRASH_CHANNEL_ID);
+    if (channel.createdBy && (channel.createdBy === OWNER_ID || channel.createdBy === BOT_ID)) {
+        return; 
+    }
+    await channel.delete();
+    const embed = new EmbedBuilder()
+        .setColor('#303135')
+        .setTitle('Protected crash by Yoku AI')
+        .setDescription('🗞 | The **channel** has been deleted by Yoku Protect');
+    await crash.send({ embeds: [embed] });
+}); 
 
-client.on('roleCreate', async (role) => {
-  const logChannel = await client.channels.fetch(LOG_CHANNEL_ID);
+client.on('roleCreate' ,async (role) => {
+  const crash = await client.channels.fetch(CRASH_CHANNEL_ID);
+  await role.delete()
   const embed = new EmbedBuilder()
-  .setColor('#303135')  // Желтый цвет для создания роли
-      .setTitle('Новая роль')
-      .setDescription(`🆕 | Created new role by ${member.name}`)
-      .setImage('https://media.discordapp.net/attachments/1014116819667259442/1114468611780784158/InShot_20230601_210530350.jpg?ex=66de0706&is=66dcb586&hm=2dd2199c9466aaf2e13089023f7c7ba612d1f3972b93eba5f1c941444a55f6bd&=&format=webp&width=1440&height=20');
-      
-  logChannel.send({ embeds: [embed] });
-});
+  .setColor('#303135')
+  .setTitle('Protected crash by Yoku AI')
+  .setDescription('🔫 | The **role** has been deleted by Yoku Protect')
+
+  crash.send({ embeds: [embed]})
+  
+}) */
 
 client.on('messageDelete', async (message) => {
   const logChannel = await client.channels.fetch(LOG_CHANNEL_ID);
-  if (message.partial) await message.fetch(); // Получаем полное сообщение, если оно частичное
+  if (message.partial) await message.fetch(); 
   const embed = new EmbedBuilder()
-  .setColor('#303135')  // Фиолетовый цвет для удаления сообщения
-      .setTitle('Сообщение удалено')
-      .setDescription(`🗑 Message deleted from the ${message.author}: __${message.content}__`)
-      .setImage('https://media.discordapp.net/attachments/1014116819667259442/1114468611780784158/InShot_20230601_210530350.jpg?ex=66de0706&is=66dcb586&hm=2dd2199c9466aaf2e13089023f7c7ba612d1f3972b93eba5f1c941444a55f6bd&=&format=webp&width=1440&height=20');
+  .setColor('#303135') 
+      .setTitle('Harmony 🌙')
+      .setDescription(`🗑 | Message deleted from the ${message.author}: __${message.content}__`);
       
   logChannel.send({ embeds: [embed] });
 });
 
 client.on('guildMemberAdd', async (member) => {
- const logChannel = await client.channels.fetch(LOG_CHANNEL_ID); 
- if (member.user.bot) {
-  const embed = new EmbedBuilder()
-  .setColor('#303135') 
-  .setTitle('Member Leave')
-  .setDescription(`🤖 Bot added ${member}`)
-  .setImage('https://media.discordapp.net/attachments/1014116819667259442/1114468611780784158/InShot_20230601_210530350.jpg?ex=66de0706&is=66dcb586&hm=2dd2199c9466aaf2e13089023f7c7ba612d1f3972b93eba5f1c941444a55f6bd&=&format=webp&width=1440&height=20');
-      
-  logChannel.send({emdebs: [emdeb]});
-}});
+  const logChannel = await client.channels.fetch(LOG_CHANNEL_ID); 
+
+  if (member.user.bot) {
+    // Блокировка бота
+    await member.ban({ reason: 'Bot automatically removed by Yoku Protect' });
+
+    const embed = new EmbedBuilder()
+      .setColor('#303135') 
+      .setTitle('Protected crash by Yoku AI')
+      .setDescription('🤖 | The **bot** has been banned by Yoku Protect');
+
+    // Исправление опечатки: должно быть "embeds", а не "emdebs"
+    logChannel.send({ embeds: [embed] });
+  }
+});
+
+
 
 (async () => {
   try {
     mongoose.set('strictQuery', false);
     await mongoose.connect(process.env.MONGODB_URI);
-    console.log('Connected to DB.');
+    console.log('✅ | Connected to DB.');
 
     eventHandler(client);
 
